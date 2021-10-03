5d685f6608bbc332e794b6f7dfcb25ccde76774c
Vasile Luta <elsile69@yahoo.com>
2021-10-04 02:10:14 +0300

    chore deps: upgrade

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.kitty-themes = {
-    url = "github:dexpota/kitty-themes";
+    url = "github:bgrnwd/everforest-kitty";
     flake = false;
   };
   outputs = { self, ... }@inputs:
