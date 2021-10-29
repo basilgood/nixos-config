0cf4c20030794ae883d7d67f5f49fb16c5265783
Luta Vasile <elsile69@yahoo.com>
2021-10-29 23:34:13 +0300

    refactor: kitty config

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -4,12 +4,17 @@
     (
       let
         config = ''
-          font_family monospace
-          font_size 10.0
+          font_family JetBrainsMonoMedium Nerd Font Mono
+          bold_font        auto
+          italic_font      auto
+          bold_italic_font auto
+          font_size 13.0
           cursor_blink_interval 0
           enable_audio_bell no
           scrollback_lines 10000
-          include ${inputs.kitty-themes}/everforest.conf
+          touch_scroll_multiplier 1.0
+          sync_to_monitor yes
+          include ${inputs.kitty-themes}/nord.conf
         '';
       in
       symlinkJoin {
