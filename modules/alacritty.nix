5efa8a7b0f845363cd68ff25cb8a3f06a4b764b7
Luta Vasile <elsile69@yahoo.com>
2021-10-29 23:42:45 +0300

    refactor: alacritty config

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -28,15 +28,15 @@
 
           font:
             normal:
-              family: "DejaVuSansMono Nerd Font"
-              style: Book
+              family: Noto Sans Mono
+              style: Regular
             bold:
-              family: "DejaVuSansMono Nerd Font"
+              family: Noto Sans Mono
               style: Bold
             italic:
-              family: "DejaVuSansMono Nerd Font"
-              style: Oblique
-            size: 10
+              family: Noto Sans Mono
+              style: Italic
+            size: 12
             offset:
               x: 0
               y: 0
@@ -48,31 +48,28 @@
 
           colors:
             primary:
-              background: '0x181818'
-              foreground: '0xd8d8d8'
-            cursor:
-              text: '0xd8d8d8'
-              cursor: '0xd8d8d8'
+              background: '0x2E3440'
+              foreground: '0xD8DEE9'
             normal:
-              black:   '0x181818'
-              red:     '0xab4642'
-              green:   '0xa1b56c'
-              yellow:  '0xf7ca88'
-              blue:    '0x7cafc2'
-              magenta: '0xba8baf'
-              cyan:    '0x86c1b9'
-              white:   '0xd8d8d8'
+              black:   '0x3B4252'
+              red:     '0xBF616A'
+              green:   '0xA3BE8C'
+              yellow:  '0xEBCB8B'
+              blue:    '0x81A1C1'
+              magenta: '0xB48EAD'
+              cyan:    '0x88C0D0'
+              white:   '0xE5E9F0'
             bright:
-              black:   '0x585858'
-              red:     '0xab4642'
-              green:   '0xa1b56c'
-              yellow:  '0xf7ca88'
-              blue:    '0x7cafc2'
-              magenta: '0xba8baf'
-              cyan:    '0x86c1b9'
-              white:   '0xf8f8f8'
+              black:   '0x4C566A'
+              red:     '0xBF616A'
+              green:   '0xA3BE8C'
+              yellow:  '0xEBCB8B'
+              blue:    '0x81A1C1'
+              magenta: '0xB48EAD'
+              cyan:    '0x8FBCBB'
+              white:   '0xECEFF4'
 
-          background_opacity: 0.90
+          background_opacity: 1
 
           cursor:
             style: Block
