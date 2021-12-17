6802a62d70e152cc557b67065d8c95f17871209e
Luta Vasile <elsile69@yahoo.com>
2021-12-17 16:14:29 +0200

    chore: bashrc history

--- hosts/plumfive/default.nix
+++ hosts/plumfive/default.nix
@@ -5,7 +5,8 @@ _:
     ../../modules/nix.nix
     ../../modules/user.nix
     # ../../modules/plasma.nix
-    ../../modules/i3wm.nix
+    # ../../modules/i3wm.nix
+    ../../modules/i3xfce.nix
     ../../modules/bashrc.nix
     ../../modules/alacritty.nix
     ../../modules/kitty.nix
@@ -22,5 +23,6 @@ _:
     ../../modules/nvim.nix
     ../../modules/tmux.nix
     ../../modules/lxc.nix
+    ../../modules/wine.nix
   ];
 }
