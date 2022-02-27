632bfcaad28bc19bd2b08546ac06bd1514eab336
Luta Vasile <elsile69@yahoo.com>
2022-02-27 19:33:49 +0200

    chore: nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.kitty-themes = {
     url = "github:connorholyday/nord-kitty";
     flake = false;
