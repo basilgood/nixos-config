545d1d5fff0f0a3dbff0e8a5c428f9c7e5131a52
Luta Vasile <elsile69@yahoo.com>
2022-03-14 00:47:44 +0200

    chore: vim update

--- modules/vim.nix
+++ modules/vim.nix
@@ -7,30 +7,29 @@
         nativeBuildInputs = [ makeWrapper ];
         paths = [
           ((vim_configurable.override { python = python3; }).overrideAttrs (oldAttrs: rec {
-            version = "8.2.4375";
+            version = "8.2.4664";
             src = fetchFromGitHub {
               owner = "vim";
               repo = "vim";
               rev = "v${version}";
-              sha256 = "sha256-e+3PpCr9HSd9glTNKBbsxWjO9f/yQGre/b3/+TBxdyc=";
+              sha256 = "sha256-v0jub3bwyPS8OehU+md+TCF+YUW94UPHiXRbMWqoiF8=";
             };
           }))
         ];
         postBuild = ''
           wrapProgram $out/bin/vim --prefix PATH : ${lib.makeBinPath [
-            nodejs
             nodePackages.jsonlint
             shfmt
             shellcheck
-            rnix-lsp
             vim-vint
             nixpkgs-fmt
             statix
             yamllint
             deno
+            nodejs
           ]}
         '';
       }
-      )
-    ];
-  }
+    )
+  ];
+}
