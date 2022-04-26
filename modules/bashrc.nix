3dad5dff820c1ee1851614360c7339bce8514796
Luta Vasile <elsile69@yahoo.com>
2022-04-26 09:57:07 +0300

    chore(bashrc): updates

--- modules/bashrc.nix
+++ modules/bashrc.nix
@@ -39,7 +39,7 @@
         . ${pkgs.fzf}/share/fzf/key-bindings.bash
         . ${pkgs.git}/share/git/contrib/completion/git-completion.bash
       fi
-      export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
+      export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --ansi'
       bind -x '"\C-r": history -n; __fzf_history__'
     '';
     promptInit = ''
