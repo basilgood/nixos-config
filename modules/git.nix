302cdba9d93d7b3f022ac92f5344ce6ff74e8c3c
Luta Vasile <elsile69@yahoo.com>
2022-04-26 09:56:18 +0300

    chore(lazygit): config update

--- modules/git.nix
+++ modules/git.nix
@@ -9,14 +9,54 @@
     merge.conflictStyle = "diff3";
   };
   programs.bash.shellAliases = {
-    lazygit = "lazygit -ucf ${ ../assets/config.yml }";
     glog = "git log --graph --all --pretty=format:'%C(240)%h %Cred%d %C(244)%cr%n %C(240)%an - %C(244)%s%n'";
   };
 
   users.users.vasy.packages = with pkgs; [
     delta
     icdiff
-    lazygit
     tig
+    (
+      let config = ''
+        gui:
+          theme:
+            lightTheme: false
+            activeBorderColor:
+              - blue
+              - bold
+            inactiveBorderColor:
+              - black
+              - bold
+            optionsTextColor:
+              - blue
+            selectedLineBgColor:
+              - reverse
+              - black
+            selectedRangeBgColor:
+              - blue
+            cherryPickedCommitBgColor:
+              - blue
+            cherryPickedCommitFgColor:
+              - cyan
+        git:
+          paging:
+            colorArg: always
+            useConfig: false
+            pager: delta --paging=never --24-bit-color=never --diff-so-fancy
+        keybinding:
+          universal:
+            undo: Z
+      '';
+      in
+      symlinkJoin {
+        name = "lazygit-with-config";
+        buildInputs = [ makeWrapper ];
+        paths = [ lazygit ];
+        postBuild = ''
+          wrapProgram "$out/bin/lazygit" \
+          --add-flags "--use-config-file ${writeText "config" config}"
+        '';
+      }
+    )
   ];
 }
